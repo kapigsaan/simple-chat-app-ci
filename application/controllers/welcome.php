19c1152133da60a51194a3ec05b9d25714577247
Jhunnie Ramos <jhunnie.ramos20@gmail.com>
2017-05-22 13:24:20 +0800

    Chat App Create message/view message

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -6,35 +6,25 @@ class Welcome extends CI_Controller {
 	public function __construct()
 	{
 		parent::__construct();
-		$this->load->helper('form');
 		$this->load->model('m_chat');
 	}
 
 	public function index()
 	{
-
-		$this->load->view('welcome_message');
-
+		$data['messages'] = $this->m_chat->getAll();
+		$this->load->view('welcome_message', $data);
 
 	}
 
-	public function chat()
+	public function createMessage()
 	{
+		$data['message'] = $this->input->post('message');
+		$date = date('Y-m-d H:i:s');
+		$data['created_at'] = $date;
 
-		if ($this->input->post()) {
-			$data['message'] = $this->input->post('message');
-			$date = date('Y-m-d H:i:s');
-			$data['created_at'] = $date;
+		$ret = $this->m_chat->createMessage($data);
 
-			$ret = $this->m_chat->createMessage($data);
-
-			if ($ret) {
-				redirect('welcome/index');
-			}
-
-		}
-
-
-
+		echo json_encode('success');
 	}
+	
 }
--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,21 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
+    <meta charset="utf-8">
+    <title>Welcome to CodeIgniter</title>
 
-	<link rel="stylesheet" type="text/css" href="<?php base_url();?>assets/css/style.css">
+    <link rel="stylesheet" type="text/css" href="<?php base_url();?>assets/css/style.css">
 
-	<link rel="stylesheet" type="text/css" href="<?php base_url();?>assets/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="<?php base_url();?>assets/css/bootstrap.css">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </head>
 <body>
 
 <div class="container">
-	<br/>
-	<hr/>
-	<br/>
+    <br/>
+    <hr/>
+    <br/>
     <div class="row">
-    	<div class = "col-md-3"></div>
+        <div class = "col-md-3"></div>
         <div class="col-md-6">
             <div class="panel panel-primary">
                 <div class="panel-heading">
@@ -30,47 +32,39 @@
                 </div>
                 <div class="panel-body" style="height:500px;">
                     <ul class="chat">
-                        <li class="left clearfix"><span class="chat-img pull-left">
-                            <img src="http://placehold.it/50/55C1E7/fff&text=U" alt="User Avatar" class="img-circle" />
-                        </span>
-                            <div class="chat-body clearfix">
-                                <div class="header">
-                                    <strong class="primary-font">Jack Sparrow</strong> <small class="pull-right text-muted">
-                                        <span class="glyphicon glyphicon-time"></span>12 mins ago</small>
-                                </div>
-                                <p>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum ornare
-                                    dolor, quis ullamcorper ligula sodales.
-                                </p>
-                            </div>
-                        </li>
-                        <li class="right clearfix"><span class="chat-img pull-right">
-                            <img src="http://placehold.it/50/FA6F57/fff&text=ME" alt="User Avatar" class="img-circle" />
-                        </span>
-                            <div class="chat-body clearfix">
-                                <div class="header">
-                                    <small class=" text-muted"><span class="glyphicon glyphicon-time"></span>13 mins ago</small>
-                                    <strong class="pull-right primary-font">Bhaumik Patel</strong>
-                                </div>
-                                <p>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum ornare
-                                    dolor, quis ullamcorper ligula sodales.
-                                </p>
-                            </div>
-                        </li>
-                        
+                        <?php if ($messages) { ?>
+                            <?php foreach($messages as $message) {?>
+                                <li class="left clearfix"><span class="chat-img pull-left">
+                                    <img src="http://placehold.it/50/55C1E7/fff&text=U" alt="User Avatar" class="img-circle" />
+                                </span>
+                                    <div class="chat-body clearfix">
+                                        <div class="header">
+                                            <strong class="primary-font">Jack Sparrow</strong> <small class="pull-right text-muted">
+                                                <span class="glyphicon glyphicon-time"></span>12 mins ago</small>
+                                        </div>
+                                        <p>
+                                            <?=$message->message?>
+                                        </p>
+                                    </div>
+                                </li>
+                            <?php } ?>
+                        <?php } ?>
                     </ul>
+
                 </div>
                 <div class="panel-footer">
-                	<!-- <?php echo form_open('welcome/chat') ?> -->
-                	<form action = "<?php base_url();?>index.php/welcome/chat" method = "POST">
-	                    <div class="input-group">
-	                        <input id="btn-input" type="text" class="form-control input-sm" name = "message" placeholder="Type your message here..." />
-	                        <span class="input-group-btn">
-	                        	<input class = "btn btn-warning btn-sm" id="btn-chat" type="submit" name="Send" value="Send" />
-	                        </span>
-	                    </div>
-                    </form>	
+                        <div class="input-group">
+                            <form>
+                                <input id="btn-input" type="text" class="message form-control input-sm" name = "message" placeholder="Type your message here..."/>
+
+                                <span class="input-group-btn">
+                                    <input class = "btn btn-warning btn-sm sendMessage" id="btn-chat" type="submit" name="Send" value="Send" />
+                                </span>
+
+                            </form>
+
+                        </div>
+                    
                 </div>
             </div>
         </div>
@@ -82,3 +76,37 @@
 
 </body>
 </html>
+
+<script type="text/javascript">
+    $(document).ready(function(){
+        $(".sendMessage").on('click', function(e){
+            e.preventDefault();
+            var message = $(".message").val();
+            var html = '<li class="left clearfix"><span class="chat-img pull-left"><img src="http://placehold.it/50/55C1E7/fff&text=U" alt="User Avatar" class="img-circle" /></span><div class="chat-body clearfix"><div class="header"><strong class="primary-font">User</strong> <small class="pull-right text-muted"><span class="glyphicon glyphicon-time"></span>12 mins ago</small></div><p>'+message+'.</p></div></li>';
+
+            if (message != '')
+            {
+                $(".panel-body .chat").append(html);
+                $(".message").val('');
+
+                 $.ajax({
+                     type: "POST",
+                     url: "index.php/welcome/createMessage",
+                     dataType: 'json',
+                     data: {message: message},
+                     success:function(data){
+                        console.log(data);
+                     },
+                     error: function(xhr, status, errorThrown){
+                        console.log(errorThrown);
+                        console.log(status);
+                        console.log(xhr);
+                       console.log(arguments);
+                       alert('request failed');
+                    }
+                });
+
+            }
+        });
+    });
+</script>
