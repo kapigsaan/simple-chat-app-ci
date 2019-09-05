45c266f579b9de4b640ffa0f58d72332c0692ff4
Arnie Jothan Earl <arniedayaoen@gmail.com>
2019-09-05 13:47:27 +0800

    Update - add googlePlus.php

---
+++ application/config/googleplus.php
@@ -0,0 +1,9 @@
+<?php
+
+	defined('BASEPATH') OR exit('No direct script access allowed');
+	$config['googleplus']['application_name'] = 'chat app';
+	$config['googleplus']['client_id']        = '467570355054-eqfsmeo4ik9jdgfpftjeilbo94r2ob5c.apps.googleusercontent.com';
+	$config['googleplus']['client_secret']    = '-9T8YZCnLNbUV54uMuVeec7p';
+	$config['googleplus']['redirect_uri']     = 'http://localhost/chat-app2/';
+	$config['googleplus']['api_key']          = 'AIzaSyDsKaXTjfG1Dqj1WeNUELRONL_X6r_tKKg';
+	$config['googleplus']['scopes']           = array("email", "profile");
